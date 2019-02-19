686a26d1fae07393114c4c783a294753f9b6ac9b
Emiliano Castellanos <castellanos.emiliano@gmail.com>
2019-02-19 03:44:17 -0300

    auth & registration using bcrypt hash

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require ('express');
 const bodyParser = require ('body-parser');
+const bcrypt = require ('bcrypt-nodejs');
 
 const app = express();
 app.use(bodyParser.json())
@@ -45,25 +46,43 @@ app.get('/', (req,res) => {
 });
 
 app.post('/signin', (req,res) => {
-    if(req.body.email == database.users[0].email && 
-        req.body.password == database.users[0].password){
-            res.json("success");
+    const {email,password} = req.body;
+
+    const user = database.users.filter( (user) => {
+        if(user.email===email){
+            return user;
+        }
+    });
+
+    if (user.length!==0){
+        bcrypt.compare(password, user[0].password, function(err, compareResponse) {
+            if (compareResponse){
+                res.json("success");
+            }
+        });
     }else {
-        res.json("error logging in");
+        res.status(400).json("password and user did not match, you assface");
     }
     
 });
 
 app.post('/register/', (req,res) => {
     const {name,email,password} = req.body;
-    database.users.push({
-        id: '111',
-        name: name,
-        email: email,
-        password: password,
-        entries: 0,
-        joined: new Date()
+    const passHashed = '';
+    bcrypt.hash(password, null, null, function(err, hash) {
+        // Store hash in your password DB.
+        database.users.push({
+            id: '111',
+            name: name,
+            email: email,
+            password: hash,
+            entries: 0,
+            joined: new Date()
+        });
+        console.log (database.users);
     });
+    
+    
     
     res.json(database.users[database.users.length-1]);
 });
@@ -101,3 +120,33 @@ app.put('/image', (req,res) => {
 /image              --> PUT = user ¿?
 
 */
+
+/*
+WAY TO CREATE A SUPERSECURE PASSWORD & LOGIN / AUTHETICATION
+
+import bcrypt from'bcrypt'
+const saltRounds = 10 // increase this if you want more iterations  
+const userPassword = 'supersecretpassword'  
+const randomPassword = 'fakepassword'
+
+const storeUserPassword = (password, salt) =>  
+  bcrypt.hash(password, salt).then(storeHashInDatabase)
+
+const storeHashInDatabase = (hash) => {  
+   // Store the hash in your password DB
+   return hash // For now we are returning the hash for testing at the bottom
+}
+
+// Returns true if user password is correct, returns false otherwise
+const checkUserPassword = (enteredPassword, storedPasswordHash) =>  
+  bcrypt.compare(enteredPassword, storedPasswordHash)
+
+
+// This is for demonstration purposes only.
+storeUserPassword(userPassword, saltRounds)  
+  .then(hash =>
+    // change param userPassword to randomPassword to get false
+    checkUserPassword(userPassword, hash)
+  )
+  .then(console.log)
+  .catch(console.error) */
